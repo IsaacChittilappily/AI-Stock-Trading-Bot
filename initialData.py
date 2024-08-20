73640b721bb1af886a0d1d178c9e47977ea74b79
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2024-08-20 17:16:19 +0100

    Added a sqlite database to store the stock data for easy access for the neural network

--- initialData.py
+++ initialData.py
@@ -11,13 +11,26 @@ alpha_api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
 symbol = 'AAPL'
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={alpha_api_key}'
 
-r = requests.get(url)
-data = r.json()
+def get_data(url: str) -> str:
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+data = get_data(url)
 time_series = data['Time Series (Daily)']
+
 df = pd.DataFrame.from_dict(time_series, orient='index')
 df.index = pd.to_datetime(df.index)
+
 one_year_ago = pd.Timestamp.now() - pd.DateOffset(years=1)
-df = df[df.index >= one_year_ago]
-df = df.sort_index()
+
+df = df[df.index >= one_year_ago].sort_index()
 
 print(df)
+db = sqlite3.connect('historical_data.db')
+cursor = db.cursor
+
+df.to_sql('Stock Prices', db, if_exists='replace', index=False)
+
+db.commit()
+db.close()

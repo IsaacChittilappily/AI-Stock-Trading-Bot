b21541aafc503e473e182d9147c14a942ed44c3b
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2024-08-20 17:06:51 +0100

    Used AlphaVantage to get the dailt stock prices for apple for the past year, converted into a pandas dataframe

--- initialData.py
+++ initialData.py
@@ -5,7 +5,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# this package automatically loads the .env file in the root directory - this ensures that the API keys are not exposed
+# this function automatically loads the .env file in the root directory - this ensures that the API keys are not exposed
 
-api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
-print(api_key)
+alpha_api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
+symbol = 'AAPL'
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={alpha_api_key}'
+
+r = requests.get(url)
+data = r.json()
+time_series = data['Time Series (Daily)']
+df = pd.DataFrame.from_dict(time_series, orient='index')
+df.index = pd.to_datetime(df.index)
+one_year_ago = pd.Timestamp.now() - pd.DateOffset(years=1)
+df = df[df.index >= one_year_ago]
+df = df.sort_index()
+
+print(df)
